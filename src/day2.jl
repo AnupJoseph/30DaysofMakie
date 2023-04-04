5de87ed42a74192545e8fa3cfef9a6a033db02ab
AnupJoseph <anup20joseph@gmail.com>
2023-04-04 13:21:36 +0000

    Finished Waffle chart

--- src/day2.jl
+++ src/day2.jl
@@ -1,4 +1,4 @@
-using DataFrames, JSONTables, HTTP, CairoMakie, StatsBase
+using DataFrames, JSONTables, HTTP, CairoMakie, StatsBase, ColorSchemes
 
 r = HTTP.get("https://raw.githubusercontent.com/AnupJoseph/30DaysofMakie/main/data/endangered_sharks.json");
 
@@ -19,11 +19,12 @@ ys = repeat(1:10, inner=10)
 points = Point2f.(xs, ys)
 colors = zeros(100)
 
+order = ["Critically Endangered", "Endangered", "Vulnerable", "Near Threatened", "Least Concern", "Data Deficient"]
 i = 1
 state = 1
-for (key, value) in percentages
+for key in order
     global state
-    for _ in 1:value
+    for _ in 1:percentages[key]
         global i, state
         colors[i] = state
         i += 1
@@ -34,12 +35,17 @@ for (key, value) in percentages
     state += 1
 
 end
-f = Figure()
-ax = Axis(f[1, 1], yreversed=true,
+f = Figure(aspect=1)
+ax = Axis(f[1:3, 1:3], yreversed=true,
     xautolimitmargin=(0.15, 0.15),
-    yautolimitmargin=(0.15, 0.15), aspect=1
+    yautolimitmargin=(0.15, 0.15),aspect=1
 )
 hidespines!(ax)
 hidedecorations!(ax)
-scatter!(points, marker=:rect, markersize=75, color=colors)
-f
+
+
+scatter!(points, marker=:rect, markersize=55, color=colors, colormap=Reverse(:OrRd_7))
+elements = [PolyElement(polycolor=reverse(colorschemes[:OrRd_7])[i]) for i in 1:length(order)]
+l = Legend(f[4, 2], elements, order, framevisible=false)
+CairoMakie.activate!(type = "png")
+save("plots/waffle.png",f)
