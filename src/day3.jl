1c7b0245d95c19daff34e3d5da48596b1e46f906
AnupJoseph <anup20joseph@gmail.com>
2023-04-06 12:29:16 +0000

    Finished up day 3

--- src/day3.jl
+++ src/day3.jl
@@ -6,11 +6,18 @@ rename!(df, "Asian elephant population (AsESG, 2019)" => "asian_elephants")
 elephants = filter(row -> row.Entity == "India", df)
 elephants = elephants[!, ["Entity", "Year", "asian_elephants"]]
 
-y_tick_format(x) = string(x) * "K"
-ytick_labels = y_tick_format.(8:2:30)
 
-f = Figure()
-Axis(f[1, 1], yticks=ytick_labels)
-lines!(elephants[!, :Year], elephants[!, :asian_elephants])
-f
-# elephants = df[,:]
+k_suffix(x) = map(i -> i >= 10000 ? string(i)[1:2] * "K" : string(i)[1] * "K", x)
+layout_color = "#e8edd9"
+ys = [i for i in collect(elephants[!, :asian_elephants])]
+
+
+f = Figure(backgroundcolor=layout_color,resolution=(1000,600))
+ax = Axis(f[1, 1], yticks=8000:2000:30000, ytickformat=k_suffix, ygridcolor="white", ygridwidth=0.5, backgroundcolor=layout_color, xticks=1980:2017, xticklabelrotation=pi / 2, xgridvisible=false)
+ylims!(8000, 30000)
+hidespines!(ax)
+
+scatterlines!(elephants[!, :Year], elephants[!, :asian_elephants], linewidth=5, color=ys,colormap=:Greens_9,markercolor="black",markersize=8)
+
+CairoMakie.activate!(type = "png")
+save("plots/flora_and_fauna.png",f)
